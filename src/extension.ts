7d09c4a7b50d539bfdf5d36ef49845258d0af3f8
stone926 <inforouter@126.com>
2022-04-09 11:05:06 +0800

    修复面tintex lint的漏洞

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode';
 import definitionProvider from './providers/definitionProvider';
 import completionProvider from './providers/completionProvider';
-import textureVariantDefinitionProvider from './providers/textureVariantDefinitionProvider';
 import citDefinitionProvider from './providers/citDefinitionProvider';
 import renameProvider from './providers/renameProvider';
 
@@ -10,10 +9,6 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(vscode.languages.registerDefinitionProvider('json', {
     provideDefinition: definitionProvider,
   }));
-
-  // context.subscriptions.push(vscode.languages.registerDefinitionProvider('json', {
-  //   provideDefinition: textureVariantDefinitionProvider
-  // }));
 
   context.subscriptions.push(vscode.languages.registerDefinitionProvider('properties', {
     provideDefinition: citDefinitionProvider
@@ -27,4 +22,5 @@ export function activate(context: vscode.ExtensionContext) {
   // 	provideRenameEdits: renameProvider,
   // }))
 }
+
 export function deactivate() { }
